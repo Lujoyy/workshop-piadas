87a21e1784d70206d15a5bad5760b804f75b4464
Lucas <lucdavi_@hotmail.com>
2022-09-16 17:49:09 -0300

    chuck norris e dad joke

---
+++ src/components/chuckNorris.jsx
@@ -0,0 +1,31 @@
+import {usaEffect, useEffect, useState} from "react";
+
+const ChuckNorris = () => {
+  const [chuck, setChuck] = useState("Fact Chuck");
+
+  useEffect(() =>{
+    getChuck();
+  },[]);
+
+  const getChuck = async () => {
+    const response = await fetch("https://api.chucknorris.io/jokes/random", {
+      method : 'GET',
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const data = await response.json();
+    setChuck(data.value);
+  };
+  return (
+    <div className="container">
+      <h1>Chuck Norris Facts</h1>
+      <p className="joke">{chuck}</p>
+      <button type="button" className="btn" onClick={getChuck}>
+        Get Another Fact
+      </button>
+    </div>
+  );
+};
+
+export default ChuckNorris;
